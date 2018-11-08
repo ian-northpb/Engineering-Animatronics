ea3139c948139ca4da60a26872dbb1da3d44ee61
Ian <ian@northpb.com>
2018-11-07 16:10:09 -0800

    Cleaned up a little bit

--- Engineering-Animatronics.ino
+++ Engineering-Animatronics.ino
@@ -15,50 +15,50 @@ int FlameOn = 0; //Declare flame on or off
 int PresentIn = 0; //Declare presents in or out
 
 void setup() // put your setup code here, to run once:
-{ 
-pinMode(FlameSensorPin, INPUT);
-//pinMode(/*Present Sensor Pin*/, INPUT);
-pinMode(LEDFrontPin, OUTPUT);
-pinMode(LEDBackPin, OUTPUT);
-pinMode(MotorPin, OUTPUT);
-//pinMode(Spotlight, OUTPUT);
+{
+  pinMode(FlameSensorPin, INPUT);
+  //pinMode(/*Present Sensor Pin*/, INPUT);
+  pinMode(LEDFrontPin, OUTPUT);
+  pinMode(LEDBackPin, OUTPUT);
+  pinMode(MotorPin, OUTPUT);
+  //pinMode(Spotlight, OUTPUT);
 }
 
 void loop() // put your main code here, to run repeatedly:
 {
-//  int PresentValue = analogRead(/*presentSensor*/);
-//  if(PresentValue > PresentThreshold)
-//  {
-//    int PresentIn = 1;  
-//  }
-//  else
-//  {
-//    int PresentIn = 0;
-//  }
+  //  int PresentValue = analogRead(/*presentSensor*/);
+  //  if(PresentValue > PresentThreshold)
+  //  {
+  //    int PresentIn = 1;
+  //  }
+  //  else
+  //  {
+  //    int PresentIn = 0;
+  //  }
   int FlameValue = analogRead(FlameSensorPin);
-  if(FlameSensorPin > FlameThreshold)
+  if (FlameSensorPin > FlameThreshold)
   {
-      int FlameOn = 1;
+    int FlameOn = 1;
   }
   else
   {
-      int FlameOn = 0;
+    int FlameOn = 0;
   }
-  if(FlameOn == 1 && PresentIn == 0)
+  if (FlameOn == 1 && PresentIn == 0)
   {
     digitalWrite(LEDFrontPin, HIGH);
     digitalWrite(LEDBackPin, LOW);
     digitalWrite(Spotlight, LOW); //Turns off spotlight, maybe use spotlight?
     analogWrite(MotorPin, 0);
   }
-  else if(FlameOn == 0 && PresentIn == 1)
+  else if (FlameOn == 0 && PresentIn == 1)
   {
     digitalWrite(LEDFrontPin, LOW);
     digitalWrite(LEDBackPin, HIGH);
     digitalWrite(Spotlight, LOW); //Turns off spotlight, maybe use spotlight?
     analogWrite(MotorPin, 0);
   }
-  else if(FlameOn == 1 && PresentIn == 1)
+  else if (FlameOn == 1 && PresentIn == 1)
   {
     digitalWrite(LEDFrontPin, HIGH);
     digitalWrite(LEDBackPin, HIGH);
@@ -72,5 +72,5 @@ void loop() // put your main code here, to run repeatedly:
     digitalWrite(Spotlight, LOW); //Turns off spotlight, maybe use spotlight?
     analogWrite(MotorPin, 0);
   }
-  
+
 }
