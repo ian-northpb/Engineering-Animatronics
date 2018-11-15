944ce02d7f87f849eea4731be7f41b8ccba665d6
Ian <ian@northpb.com>
2018-11-15 11:56:10 -0800

    Finishing rough case statement, need to scroll and blink lcd to look cooler

--- Engineering-Animatronics.ino
+++ Engineering-Animatronics.ino
@@ -1,6 +1,5 @@
 /*Animatronic Master Code Holiday Themed PreCode Testing*/
 
-
 /*To Do List
  * Make LCD Say Merry Christmas and Ho Ho Ho while not doign anythign
  * 
@@ -10,88 +9,139 @@
  */
 //Included Libraries
   #include <SoftwareSerial.h> //Library to use serial monitor
-  #include <LiquidCrystal.h> //Library to use LCD Display
   #include <Servo.h> //Library to use and control Servos
-  #include <RobotIRremoteInt.h>
-  #include <RobotIRremote.h>
-  #include <RobotIRremoteTools.h>
+  #include <IRremote.h> //Library to use and control infrared
+  #include <LiquidCrystal_I2C.h>
 //Flame Sensor Variable Setup
   int const FlameSensorPin = A0; //Declare Flame Sensor Pin
-  int const FlameThreshold = 100; //Declare threshold for flame considered on
+  int const FlameThreshold = 400; //Declare threshold for flame considered on
   int FlameValue = 0; //Declare value of flame, later used to determine on or off
   bool FlameOn = false; //Declare flame on or off
 //Motor Variable Setup
   int const MotorPin = A1; //Declare pin slot for motor
+  int const TrainRunStop = 0;
+  int const TrainRunSpeed = 100;
 //Spotlight Variable Setup
   int const Spotlight = 2; //Declare pin for LED on front, act as a spotlight
 //Remote Control Variable Setup
   int const IRremotePin = 11; //Declare pin for Infared Remote receiver
-  int IRValue = 0; //Need to learn how to use this!!!! Remote and Receiver
+  unsigned long IRValue = 0x000000; //Need to learn how to use this!!!! Remote and Receiver
   bool TrainRun = false; //use on/off button on remote to toggle true and false
-//Nice Servo Variable Setup
-  Servo NiceServo;
-  int const NiceServoPin = 9; //Declare pin of servo controlling nice presents
-  int NiceServoStartPos = 0; //Starting position of the servo
-  int NiceServoPourPos = 90; //Position to dump present
-//Naughty Servo Variable Setup
-  Servo NaughtyServo;
-  int const NaughtyServoPin = 10; //Declare pin of servo controlling coal
-  int NaughtyServoStartPos = 0; //Starting position of the servo
-  int NaughtyServoPourPos = 90; //Position to dump coal
+  unsigned long const PowerButtonHex = 0xFD00FF; //Hex Code for power button, button labled power
+  unsigned long const NiceButtonHex = 0xFFFAFA; //Hex Code for nice button, button labeled
+  unsigned long const NaughtyButtonHex = 0xF0FFF0; //Hex Code for naughty button, button labeled
+  IRrecv irrecv(IRremotePin);
+  decode_results results;
+//Servo Variable Setup
+  Servo PresentServo;
+  int const PresentServoPin = 9; //Declare pin of servo controlling nice presents
+  int const ServoDefaultPos = 90; //Starting position of the servo
+  int const ServoNicePourPos = 180; //Position to dump present
+  int const ServoNaughtyPourPos = 0; //Position to dump coal
+  int ServoPos = 90;
 //LCD Variable Setup
+  LiquidCrystal_I2C lcd(0x20, 16, 2); // set the LCD address to 0x20(Cooperate with 3 short circuit caps) for a 16 chars and 2 line display
   //Werid Setup, figure it out
 
 void setup() // put your setup code here, to run once:
 {
+//Serial Setup
+  Serial.begin(9600);
 //Flame Sensor Setup
   pinMode(FlameSensorPin, INPUT); //Assigns pin for the Flame Sensor
 //Remote Control Setup
-  pinMode(IRremotePin, INPUT); //Assigns pin for the Infared Remote receiver
+  irrecv.enableIRIn();
 //Motor Setup
   pinMode(MotorPin, OUTPUT); //Assigns Pin for the drive motor
 //Front Spotlight Setup
   pinMode(Spotlight, OUTPUT); //Assigns pin for the LED Front light
 //LCD Screen Setup
-  
-//Nice Servo Setup
-  NiceServo.attach(NiceServoPin); //Assigns pin for the "Nice" Servo
-  NiceServo.write(NiceServoStartPos); //Sets servo to starting position
-//Naughty Servo Setup
-  NaughtyServo.attach(NaughtyServoPin); //Assigns pin for the "Naughty" Servo
-  NaughtyServo.write(NaughtyServoStartPos); //Sets servo to starting position
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  lcd.home();
+//Servo Setup
+  PresentServo.attach(PresentServoPin); //Assigns pin for the "Nice" Servo
+  PresentServo.write(ServoDefaultPos); //Sets servo to starting position
 
 }
 void loop() // put your main code here, to run repeatedly:
 {
+  lcd.setCursor(3, 0);
+  lcd.print("Ho Ho Ho");
+  lcd.setCursor(0, 1);
+  lcd.print("Merry Christmas");
   int FlameValue = analogRead(FlameSensorPin); //Equates variable "FlameValue" to the data read by flame sensor
+  unsigned long IRValue = irrecv.decode(&results);
+//Turn on flame if higher than threshold
   if (FlameValue > FlameThreshold) //Do if the flame value is greater than its threshold
   {
     bool FlameOn = true; //Write FlameOn as true
   }
-
   else //Do if the flame value is less than its threshold
   {
     bool FlameOn = false; //Write FlameOn as false
   }
-  if (IRValue == 0/******POWER BUTTON*********Need to configure the remote data******************/) //Do if off button is pressed
+  
+  while (FlameOn == true) //Runs while the flame is lit
   {
-    bool TrainRun = !TrainRun; //Toggle train as on/off
-  }
-  while (FlameOn == true) //unsure if works because Bool FlameOn changes outside of loop //Do while the flame is lit
-  {
+    //display Merry Christmas
     digitalWrite(Spotlight, HIGH); //Turn on the spotlight while flame is lit
+    /*if(irrecv.decode(&results)) //Not sure if necessary*************************************************/
+    if (IRValue == PowerButtonHex) //Do if power button is pressed
+    {
+      bool TrainRun = !TrainRun; //Toggle train as on/off
+    }
     while (TrainRun == true) //Do while train was turned "on" and the flame is lit
     {
-      switch (IRValue/*not sure if we need to convert variable*/) //Use the remote to run different codes
+      //Display Merry Christmas and HO HO HO, alternating and scrolling
+      if (irrecv.decode(&results))
       {
-        case 1/*configure to a button*/: //Press __ button to run scenario
-//fill out action          //display naughty on LCD, and run servo naughty to drop black "coal"
-          break; //ends case statement if case 1 is run
-        case 2/*configure to a button*/: //Press __ button to run scenario
-//fill out action          //display nice on LCD, and run servo nice to drop colorful present
-          break; //ends case statement if case 2 is run
-        default: //default action if neither case 1 nor case 2 happens
-          break; //ends code if default code runs
+        switch (IRValue) //Use the remote to run different codes
+        {
+          case NaughtyButtonHex: //Press __ button to run scenario
+            analogWrite(MotorPin, TrainRunStop);
+            lcd.setCursor(0, 0);
+            lcd.print("Naughty");
+            lcd.setCursor(1, 9);
+            lcd.print("Naughty");
+            for (; ServoPos <= ServoNaughtyPourPos; ServoPos = ServoPos + 1)
+            {
+              analogWrite(PresentServoPin, ServoPos);
+              delay(20);
+            }
+            delay(1000);
+            for (; ServoPos >= ServoDefaultPos; ServoPos = ServoPos - 1)
+            {
+              analogWrite(PresentServoPin, ServoPos);
+              delay(20);
+            }
+            delay(1000);
+            analogWrite(MotorPin, TrainRunSpeed);
+            break; //ends case statement if case 1 is run
+          case NiceButtonHex: //Press __ button to run scenario
+            lcd.setCursor(0, 0);
+            lcd.print("Nice!!");
+            lcd.setCursor(10, 1);
+            lcd.print("Nice!!");
+            analogWrite(MotorPin, TrainRunStop);
+            for (; ServoPos >= ServoNicePourPos; ServoPos = ServoPos - 1)
+            {
+              analogWrite(PresentServoPin, ServoPos);
+              delay(20);
+            }
+            delay(1000);
+            for (; ServoPos <= ServoDefaultPos; ServoPos = ServoPos + 1)
+            {
+              analogWrite(PresentServoPin, ServoPos);
+              delay(20);
+            }
+            delay(1000);
+            analogWrite(MotorPin, TrainRunSpeed);
+            break; //ends case statement if case 2 is run
+          default: //default action if neither case 1 nor case 2 happens
+            break; //ends code if default code runs
+        }
+        irrecv.resume();
       }
     }
   }
